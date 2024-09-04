a3e44de2377efbb3f655bda303659398405031ef
likan stha <likan.stha@gmail.com>
2024-09-04 13:31:59 +1000

    swagger openapi implementation

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-import express from 'express';
-import { healthRouter, calculatorRouter } from './routes';
-import { addTimestamp, errorHandler, logger } from './middlewares';
+import express from "express";
+import { healthRouter, calculatorRouter, apiDocsRouter } from "./routes";
+import {
+  addTimestamp,
+  errorHandler,
+  logger,
+  openApiValidator,
+} from "./middlewares";
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(addTimestamp);
 app.use(logger);
+app.use(openApiValidator);
 
-app.use('/health', healthRouter);
-app.use('/calculator', calculatorRouter);
+app.use("/api-docs", apiDocsRouter);
+app.use("/health", healthRouter);
+app.use("/calculator", calculatorRouter);
 
 app.use(errorHandler);
 
--- src/types/calculator.ts
+++ src/types/calculator.ts
@@ -3,8 +3,8 @@ import { components } from "../schemas";
 export type CalculatorCommandDto = components["schemas"]["CalculatorCommand"];
 
 export type CalculatorResultDto = components["schemas"]["CalculatorResult"];
-// export interface CalculatorRequestBody {
-//   operator: '+' | '-' | '*' | '/';
-//   operand1: number;
-//   operand2: number;
-// }
+export interface CalculatorRequestBody {
+  operator: "+" | "-" | "*" | "/";
+  operand1: number;
+  operand2: number;
+}
